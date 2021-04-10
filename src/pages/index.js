d3b6bd48fd76d4f4bdd7370eb644f9fedfafccde
Adipati Ma'ruf Alamsyah <adipati.cs.samsung@gmail.com>
2021-04-10 11:50:32 +0700

    add input component & material-ui/(core+icons)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,20 @@
 import Head from 'next/head';
 import Layout from '../components/Layout/layout';
+import SearchInput from '../components/SearchInput/SearchInput';
 import styles from '../styles/Home.module.css';
 
 export default function Home({ countries }) {
   console.log(countries);
 
-  return <Layout>maine</Layout>;
+  return (
+    <Layout>
+      <div className={styles.counts}>Found {countries.length} countries</div>
+      <SearchInput placeholder="Filter by Name, Region, or SubRegion" />
+    </Layout>
+  );
 }
 
-export const getStaticprops = async () => {
+export const getStaticProps = async () => {
   const res = await fetch(`https://restcountries.eu/rest/v2/all`);
   const countries = await res.json();
 
