b5b7fcf6e7bbee7b0a5104a2b4c301c7e3f68e50
Adipati Ma'ruf Alamsyah <adipati.cs.samsung@gmail.com>
2021-04-19 14:31:32 +0700

    fix bug on vercel deploy(2)

--- src/pages/country/[id].js
+++ src/pages/country/[id].js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import Layout from '../../components/Layout/Layout';
+import Layout from '../../components/Layout';
 import styles from './country.module.css';
 
 const getCountry = async (id) => {
